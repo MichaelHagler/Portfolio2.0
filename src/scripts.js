32fc97d9850faf8575625ece1f2864bd2822a152
MichaelHagler <67695612+MichaelHagler@users.noreply.github.com>
2024-08-19 08:06:33 -0700

    app info
    added a quick description of app and started adding the links to the github and later a link to the live app

--- src/scripts.js
+++ src/scripts.js
@@ -1,12 +1,21 @@
 let showInfo = document.getElementById("show-info");
 let para = document.createElement("p");
-let toDoListInfo = document.createTextNode("This is a summary of my app");
+
 
 const toDoListBtn = document.getElementById("to-do-list-app");
 
 document.addEventListener("DOMContentLoaded", () => {
+  const toDoListInfo = document.createTextNode("A straight forward to-do-list app created using HTML, CSS, and JavaScript. Add items, cross them out, or remove them from the list entirely.");
+  let appLink = document.createElement("a");
+
+  appLink.href = "https://github.com/MichaelHagler?tab=repositories";
+  appLink.target = "_blank";
+  appLink.textContent = "My GitHub";
+
   toDoListBtn.addEventListener("click", () => {
+
     para.appendChild(toDoListInfo);
     showInfo.appendChild(para);
+    showInfo.appendChild(appLink);
   })
 });
